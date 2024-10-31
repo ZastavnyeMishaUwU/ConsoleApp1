48e300cb28ddd86cc1f0433b3ddbc3bcb3e3513b
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-10-31 21:21:58 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Веди число  N: ");
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
